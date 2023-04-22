209fd60a0f8e72d12f3ba0261a9fabdf8069d061
Joana Elise <112410414+joanaeliseal@users.noreply.github.com>
2023-04-22 17:43:47 -0300

    first commit

---
+++ estrutura_de_repeticao/ex1.py
@@ -0,0 +1,4 @@
+num = int(input('Informe quantos números você quer: '))
+
+for i in range(1,num+1):
+    numero = int(input(f'Digite {i}º número: '))
---
+++ estrutura_de_repeticao/q4.py
@@ -0,0 +1,2 @@
+# Faça um programa que leia 20 números inteiros, determine e mostre o maior deles.
+
